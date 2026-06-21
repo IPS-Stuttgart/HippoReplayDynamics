6b72d9cd4e8edb1fa986fdb424f082834fe18514
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-06-21 20:48:19 +0200

    Test integer-valued cell ID metadata

--- tests/test_ground_truth_cell_id_metadata.py
+++ tests/test_ground_truth_cell_id_metadata.py
@@ -1,2 +1,8 @@
-def test_strict_cell_id_metadata_smoke():
-    assert True
+import numpy as np
+
+from hipporeplayimm.ground_truth import _parse_cell_ids
+
+
+def test_parse_cell_ids_accepts_integer_valued_metadata():
+    np.testing.assert_array_equal(_parse_cell_ids("1 2.0 3"), np.array([1, 2, 3]))
+    np.testing.assert_array_equal(_parse_cell_ids([1, 2.0, np.int64(3)]), np.array([1, 2, 3]))

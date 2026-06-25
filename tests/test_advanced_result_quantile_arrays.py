a94ed49a84f1c078853f87c33d527bbd2da7ffca
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-06-25 23:03:16 +0200

    Ignore nonfinite diagnostic quantile inputs (#561)
    * Ignore nonfinite diagnostic quantile inputs
    
    * Test nonfinite diagnostic quantile inputs

--- tests/test_advanced_result_quantile_arrays.py
+++ tests/test_advanced_result_quantile_arrays.py
@@ -12,3 +12,11 @@ def test_advanced_diagnostics_quantile_accepts_numpy_arrays():
 def test_advanced_diagnostics_quantile_empty_inputs_return_nan():
     assert np.isnan(_quantile([], 0.5))
     assert np.isnan(_quantile(np.array([], dtype=float), 0.5))
+
+
+def test_advanced_diagnostics_quantile_ignores_nonfinite_values():
+    assert _quantile(np.array([np.nan, 1.0, np.inf, 3.0]), 0.5) == 2.0
+
+
+def test_advanced_diagnostics_quantile_all_nonfinite_values_return_nan():
+    assert np.isnan(_quantile(np.array([np.nan, np.inf, -np.inf]), 0.5))

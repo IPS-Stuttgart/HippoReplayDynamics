3952b5db4af948043701421ff27fbfd03c6adb67
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-06-25 14:52:49 +0200

    Fix advanced diagnostic quantile helper for array inputs (#534)
    * Fix advanced quantile helper for array inputs
    
    * Apply advanced quantile array patch at import
    
    * Add regression test for quantile array inputs

--- src/hipporeplayimm/__init__.py
+++ src/hipporeplayimm/__init__.py
@@ -3,6 +3,7 @@
 
 from . import accuracy_model_probability_status_patch as _accuracy_model_probability_status_patch
 from . import advanced_result_empty_threshold_patch as _advanced_result_empty_threshold_patch
+from . import advanced_result_quantile_array_patch as _advanced_result_quantile_array_patch
 from . import advanced_result_status_patch as _advanced_result_status_patch
 from . import advanced_result_threshold_validation as _advanced_result_threshold_validation
 from . import advanced_result_wrong_map_bootstrap_patch as _advanced_result_wrong_map_bootstrap_patch
@@ -255,6 +256,7 @@ def apply_runtime_patches() -> None:
     _advanced_result_empty_threshold_patch.apply_advanced_result_empty_threshold_patch()
     _advanced_result_threshold_validation.apply_advanced_result_threshold_validation_patch()
     _advanced_result_wrong_map_bootstrap_patch.apply_wrong_map_rat_bootstrap_patch()
+    _advanced_result_quantile_array_patch.apply_advanced_result_quantile_array_patch()
     _latent_path_validation.apply_latent_path_validation_patch()
     _simulation_recovery_runtime_limits.apply_simulation_recovery_runtime_limit_validation_patch()
     _simulation_recovery_count_validation.apply_simulation_recovery_count_validation_patch()

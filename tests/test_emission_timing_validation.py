6502878b0bc1a2d6e85c1ae56c85f60772a3b6db
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-10 22:18:53 +0200

    Add complex log-emission regressions

--- tests/test_emission_timing_validation.py
+++ tests/test_emission_timing_validation.py
@@ -33,6 +33,24 @@ def test_log_emission_tensor_rejects_boolean_timing_metadata(field, value):
         LogEmissionTensor(**_tensor_kwargs(**{field: value}))
 
 
+@pytest.mark.parametrize(
+    ("field", "value"),
+    [
+        ("log_likelihood", np.array([[0.0 + 1.0j], [0.0 + 0.0j]])),
+        ("spike_counts", np.array([[0.0 + 1.0j], [0.0 + 0.0j]])),
+        ("times", np.array([0.0 + 1.0j, 1.0 + 0.0j])),
+        ("dt", np.complex128(1.0 + 1.0j)),
+        ("cell_ids", np.array([1.0 + 1.0j])),
+        ("n_spikes", np.complex128(0.0 + 1.0j)),
+        ("bin_durations", np.array([1.0 + 1.0j, 1.0 + 0.0j])),
+        ("transition_durations", np.array([1.0 + 1.0j])),
+    ],
+)
+def test_log_emission_tensor_rejects_complex_metadata_before_float_coercion(field, value):
+    with pytest.raises(ValueError, match=rf"{field}.*complex"):
+        LogEmissionTensor(**_tensor_kwargs(**{field: value}))
+
+
 def test_log_emission_tensor_accepts_numeric_timing_metadata():
     emissions = LogEmissionTensor(**_tensor_kwargs())
 
